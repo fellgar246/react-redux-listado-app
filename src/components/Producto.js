80d9655949e92c4764b36ed7bc364c8cb94ba625
fellgar246 <fellgar246@gmail.com>
2023-01-16 13:46:53 -0600

    first commit

---
+++ src/components/Producto.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+// Redux
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction, obtenerProductoEditar } from '../actions/productoActions';
+
+const Producto = ({producto}) => {
+    const { nombre, precio, id } = producto;
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate(); // habilitar history para redirección, ahora es useNavigate
+
+    // Confirmar si desea eliminarlo
+    const confirmarEliminarProducto = id => {
+
+        // preguntar al usuario
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Un producto que se elimina no se puede volver a eliminar",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText: 'Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+             // pasarlo al action
+                dispatch( borrarProductoAction(id) );
+            }
+          })
+
+    }
+
+    const redireccionarEdicion = producto => {
+        dispatch( obtenerProductoEditar(producto) );
+        navigate.push(`/productos/aditar/${producto.id}`)
+    }
+
+
+  return (
+    <tr>
+        <td>{ nombre }</td>
+        <td><span className='font-weight-bold'>{ precio }</span></td>
+        <td className='acciones'>
+            <button 
+                type="button"
+                onClick={() => redireccionarEdicion(producto)}
+                className='btn btn-primary mr-2'
+            >
+                Editar
+            </button>
+            <button
+                type='button'
+                className='btn btn-danger'
+                onClick={() => confirmarEliminarProducto(id)}
+            >Eliminar</button>
+        </td>
+    </tr>
+  )
+}
+
+export default Producto
